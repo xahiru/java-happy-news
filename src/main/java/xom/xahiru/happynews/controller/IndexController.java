706b16543c8553901dd25dc245b53b793e142ac7
Ahmed Zahir <xahiru@gmail.com>
2015-09-25 16:09:21 +0800

    Tiles 3 working perfectly after 18 hours of debugging

---
+++ src/main/java/xom/xahiru/happynews/controller/IndexController.java
@@ -0,0 +1,16 @@
+package xom.xahiru.happynews.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index(){
+		
+		return "index";
+	}
+
+}
+
