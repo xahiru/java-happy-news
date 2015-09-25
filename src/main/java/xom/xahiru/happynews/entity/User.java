59745409d342b4682384311d81d260443cbf7b12
Ahmed Zahir <xahiru@gmail.com>
2015-09-26 00:21:57 +0800

    jpa entities added, blog,items,user,and roles

--- src/main/java/xom/xahiru/happynews/entity/User.java
+++ src/main/java/xom/xahiru/happynews/entity/User.java
@@ -1,8 +1,13 @@
 package xom.xahiru.happynews.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User {
@@ -10,6 +15,62 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	@ManyToMany
+	@JoinTable
+	private List<Role> roles;
+	
+	@OneToMany(mappedBy="user")
+	private List<Blog> blogs;
+	
+	
+	
+
+	public List<Blog> getBlogs() {
+		return blogs;
+	}
+
+	public void setBlogs(List<Blog> blogs) {
+		this.blogs = blogs;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Integer getId() {
 		return id;
